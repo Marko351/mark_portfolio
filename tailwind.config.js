5a74d02492d521f21c52d17fd48677ed415f1b0f
Marko351 <markodjuric351@gmail.com>
2022-12-21 23:35:43 +0100

    layout component in progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#64ffda',
+        'primary-light': '#D8E9A8',
+        background: '#191A19',
+        'background-light': '#2F3030',
+      },
+      fontFamily: {
+        SpaceMono: ['Space Mono', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
